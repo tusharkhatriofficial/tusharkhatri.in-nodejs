b06d7c6782787299d9ddaca498eacedafe0addba
Tushar Khatri <khatritushar320@gmail.com>
2022-09-03 19:22:49 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,43 @@
+//imports
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+//app.use
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+//routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+
+
+
+//listen to port
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is up and running on port 3000");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
